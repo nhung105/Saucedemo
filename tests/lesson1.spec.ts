bd9673e7d98ae8847cd27d3c4c6dde75d37ae688
NhungBeo <nguyennhung105124@gmail.com>
2025-08-20 22:11:29 +0700

    edit wrong testcase

--- tests/lesson1.spec.ts
+++ tests/lesson1.spec.ts
@@ -16,11 +16,25 @@ test("the product list contains exactly six items", async ({ page }) => {
   await expect(page.locator(".inventory_item")).toHaveCount(6);
 });
 
-test("each product in the list displays its price", async ({ page }) => {
-  const prices = await page.locator(".inventory_item_price").allTextContents();
-  prices.forEach((price) => {
-    expect(price.trim()).toMatch(/^\$/);
-  });
+test("The item should display correct correct price value", async ({
+  page,
+}) => {
+  const products = [
+    { name: "Sauce Labs Backpack", price: "$29.99" },
+    { name: "Sauce Labs Bike Light", price: "$9.99" },
+    { name: "Sauce Labs Bolt T-Shirt", price: "$15.99" },
+    { name: "Sauce Labs Fleece Jacket", price: "$49.99" },
+    { name: "Sauce Labs Onesie", price: "$7.99" },
+    { name: "Test.allTheThings() T-Shirt (Red)", price: "$15.99" },
+  ];
+  for (const { name, price } of products) {
+    const actualPrices = await page
+      .locator(".inventory_item")
+      .filter({ hasText: name })
+      .locator(".inventory_item_price")
+      .textContent();
+    expect(actualPrices?.trim()).toEqual(price);
+  }
 });
 
 test("the product is successfully added to the cart", async ({ page }) => {
